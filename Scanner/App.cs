45dd6807fc5a45f9259448f2408762c5740c8dfd
selman <selmanalpdundar@hotmail.com>
2017-08-20 01:53:53 +0300

    Upload

---
+++ Scanner/App.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+namespace Scanner {
+    public partial class App : Application {
+        public App() {
+            var startPage = new PopUpPage();
+            MainPage = new NavigationPage(startPage);
+        }
+
+
+    }
+}
---
+++ Scanner/PopUpPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace Scanner {
+    public partial class PopUpPage : ContentPage {
+        public PopUpPage() {
+            InitializeComponent();
+        }
+
+        public void OnClickScan(object sender, EventArgs e) {
+            ScanAsync();
+        }
+
+        public async void ScanAsync() {
+
+            var scanPage = new ZXingScannerPage();
+
+            scanPage.OnScanResult += (result) => {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+
+            // Navigate to our scanner page
+            await Navigation.PushAsync(scanPage);
+
+        }
+    }
+}
